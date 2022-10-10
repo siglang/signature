ca1c98a1bb9b65ff99d8147e361b6409f2c17f47
tsukiroku <tsukiroku@yahoo.com>
2022-10-10 17:44:43 +0900

    Update macros.rs

--- sntk_core/src/macros.rs
+++ sntk_core/src/macros.rs
@@ -1,20 +1,4 @@
 #[macro_export]
-macro_rules! inline_if {
-    ($cond:expr; $then:expr; $else:expr) => {
-        if $cond {
-            $then
-        } else {
-            $else
-        }
-    };
-    ($cond:expr; $then:expr) => {
-        if $cond {
-            $then
-        }
-    };
-}
-
-#[macro_export]
 macro_rules! bind {
     ($($f:expr),* => $r:expr) => {{
         $( $f; )*
