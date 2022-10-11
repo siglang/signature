c85348d0b68c5268659a10c249ebb1992ff45d52
tsukiroku <tsukiroku@yahoo.com>
2022-10-11 19:47:27 +0900

    Update mod.rs

--- sntk_core/src/tokenizer/mod.rs
+++ sntk_core/src/tokenizer/mod.rs
@@ -1,2 +1,19 @@
+/// **Lexer receives the source code in the from of a string and returns tokenized data.**
+///
+/// for example:
+/// ```rust
+/// use sntk_core::tokenizer::{token::*, lexer::*};
+///
+/// let mut lexer = Lexer::new(r#"let x_32z = y != "Hello, World\n";"#);
+/// let mut token = Token::new(Tokens::ILLEGAL(String::new()), (0, 0));
+///
+/// while token.token_type != Tokens::EOF {
+///     token = lexer.next_token();
+///     println!("{}", token);
+/// }
+/// ```
 pub mod lexer;
+/// An enumeration of tokens, which are the basic units of that make up a language.
+/// 
+/// the `Token` structure containing `Tokens` and position (line, column) of the token.
 pub mod token;
