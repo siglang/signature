aa7f998a4dfe548a354f1ec6bbd8f0fbc742c900
tsukiroku <tsukiroku@yahoo.com>
2022-10-14 23:20:25 +0900

    Create error.rs

---
+++ sntk_core/src/parser/error.rs
@@ -0,0 +1,34 @@
+use super::ast::*;
+
+#[derive(Debug, PartialEq, Default, Clone)]
+pub struct ParsingError {
+    pub message: String,
+    pub position: Position,
+}
+
+impl ParsingError {
+    pub fn new(message: &str, args: Vec<&str>, position: Position) -> Self {
+        let mut message = message.to_string();
+
+        args.iter().enumerate().for_each(|(i, arg)| {
+            message = message.replace(&format!("{{{i}}}"), arg);
+        });
+
+        ParsingError { message, position }
+    }
+}
+
+macro_rules! messages {
+    ($( $name:ident => $message:expr );*;) => {
+        $(
+            pub const $name: &str = $message;
+        )*
+    };
+}
+
+messages! {
+    EXPECTED_NEXT_TOKEN => "expected next token to be {0}, got {1} instead";
+    EXPECTED_DATA_TYPE => "expected next token to be a data type, got {0} instead";
+    EXPECTED_EXPRESSION => "expected next token to be an expression, got {0} instead";
+    UNEXPECTED_TOKEN => "unexpected token {0}";
+}
