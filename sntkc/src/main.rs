9394a989d16c982473795d9705d9721075a9f568
tsukiroku <tsukiroku@yahoo.com>
2022-10-14 23:20:38 +0900

    Update main.rs

--- sntkc/src/main.rs
+++ sntkc/src/main.rs
@@ -1,7 +1,5 @@
 use sntk_core::parser::parser::*;
 
 fn main() {
-    let parsed = Parser::from("let x: T<U> = 10; return 3; 2;").parse_program();
-
-    println!("{parsed:#?}");
+    println!("{:#?}", Parser::from("let x: T<U[]> = 10; return (3); 2;").parse_program());
 }
