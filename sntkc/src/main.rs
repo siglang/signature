fcb9aee86d93d4ee77f0f368bd5446b3efc7f148
tsukiroku <tsukiroku@yahoo.com>
2022-10-14 19:06:13 +0900

    Update main.rs

--- sntkc/src/main.rs
+++ sntkc/src/main.rs
@@ -1,7 +1,8 @@
 use sntk_core::parser::parser::*;
 
 fn main() {
-    let parsed = Parser::from("let x: number = 10;").parse_program();
+    let parsed = Parser::from("let x: T<U> = 10; 3;").parse_program();
+    // TODO: not working. ^ "expected next token to be an expression, got Semicolon instead"
 
     println!("{parsed:#?}");
 }
