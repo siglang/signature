c70235be02b6c9d61ccdc5c19b1d51f6cc50ec09
tsukiroku <tsukiroku@yahoo.com>
2022-10-15 15:21:52 +0900

    Update macros.rs

--- sntk_core/src/macros.rs
+++ sntk_core/src/macros.rs
@@ -4,11 +4,16 @@ macro_rules! ident {
         match $self.current_token.token_type {
             Tokens::IDENT(ref ident) => ident.clone(),
             _ => {
-                return Err(ParsingError::new(
-                    UNEXPECTED_TOKEN,
-                    vec![&$self.current_token.token_type.stringify()],
-                    position! { $self },
-                ))
+                // return Err(ParsingError::new(
+                //     UNEXPECTED_TOKEN,
+                //     vec![&$self.current_token.token_type.stringify()],
+                //     position! { $self },
+                // ))
+                return Err(parsing_error! {
+                    $self;
+                    UNEXPECTED_TOKEN;
+                    $self.current_token.token_type
+                });
             }
         }
     };
@@ -18,5 +23,16 @@ macro_rules! ident {
 macro_rules! position {
     ($self:ident) => {
         $self.position.clone()
+    };
+}
+
+#[macro_export]
+macro_rules! parsing_error {
+    ($self:ident; $msg:ident; $( $r:expr ),*) => {
+        ParsingError::new(
+            $msg,
+            vec![$( format!("{}", $r) ),*],
+            position! { $self },
+        )
     };
 }
