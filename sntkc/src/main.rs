871ef7e943265c746d8db054e9af1bd3c112d736
tsukiroku <tsukiroku@yahoo.com>
2022-10-16 16:32:14 +0900

    Update main.rs

--- sntkc/src/main.rs
+++ sntkc/src/main.rs
@@ -5,16 +5,21 @@ fn main() {
         "{:#?}",
         Parser::from(r#"let x: T<U[], V, L> = 10; return (3); 2;"#).parse_program()
     );
-
     println!(
         "{:#?}",
         Parser::from(r#"type X<T, U> = T<>;"#).parse_program()
     );
-
     println!(
         "{:#?}",
         Parser::from(r#"type X<T, U> = fn(T, U[]) -> string;"#).parse_program()
     );
-
     println!("{:#?}", Parser::from(r#"[1, 2, 3];"#).parse_program());
+    println!(
+        "{:#?}",
+        Parser::from(r#"object {"x": 10, 2: 3};"#).parse_program()
+    );
+    println!(
+        "{:#?}",
+        Parser::from(r#"{{{{{{{{{};};};};};};};};};"#).parse_program()
+    );
 }
