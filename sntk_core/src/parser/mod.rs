16c3576d3619fdaf6aeacedc8d0122eed2a12e78
tsukiroku <tsukiroku@yahoo.com>
2022-10-16 18:01:36 +0900

    Update mod.rs

--- sntk_core/src/parser/mod.rs
+++ sntk_core/src/parser/mod.rs
@@ -1,3 +1,14 @@
+/// AST Node structures and enumerations such as expression, statements, etc are implemented in this module.
 pub mod ast;
+/// The error module contains the error handling for the language.
 pub mod error;
+/// **Parses the input string into an AST.**
+///
+/// for example:
+/// ```rust
+/// use sntk_core::parser::parser::*;
+///
+/// let parsed = Parser::from(r#"type X<T, U> = fn(T, U[]) -> object T: U;"#).parse_program();
+/// println!("{parsed:#?}");
+/// ```
 pub mod parser;
