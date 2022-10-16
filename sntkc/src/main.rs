1edd4651fab9de48297ac3174aca9e67d9b2862b
tsukiroku <tsukiroku@yahoo.com>
2022-10-16 20:56:11 +0900

    Update main.rs

--- sntkc/src/main.rs
+++ sntkc/src/main.rs
@@ -1,25 +1,40 @@
 use sntk_core::parser::parser::*;
 
 fn main() {
+    // println!(
+    //     "{:#?}",
+    //     Parser::from(r#"let x: T<U[], V, L> = 10; return (3); 2;"#).parse_program()
+    // );
+    // println!(
+    //     "{:#?}",
+    //     Parser::from(r#"type X<T, U> = T<>;"#).parse_program()
+    // );
+    // println!(
+    //     "{:#?}",
+    //     Parser::from(r#"type X<T, U> = fn(T, U[]) -> object T: U;"#).parse_program()
+    // );
+    // println!("{:#?}", Parser::from(r#"[1, 2, 3];"#).parse_program());
+    // println!(
+    //     "{:#?}",
+    //     Parser::from(r#"object {"x": 10, 2: 3};"#).parse_program()
+    // );
+    // println!(
+    //     "{:#?}",
+    //     Parser::from(r#"{{{{{{{{{};};};};};};};};};"#).parse_program()
+    // );
     println!(
         "{:#?}",
-        Parser::from(r#"let x: T<U[], V, L> = 10; return (3); 2;"#).parse_program()
-    );
-    println!(
-        "{:#?}",
-        Parser::from(r#"type X<T, U> = T<>;"#).parse_program()
-    );
-    println!(
-        "{:#?}",
-        Parser::from(r#"type X<T, U> = fn(T, U[]) -> object T: U;"#).parse_program()
-    );
-    println!("{:#?}", Parser::from(r#"[1, 2, 3];"#).parse_program());
-    println!(
-        "{:#?}",
-        Parser::from(r#"object {"x": 10, 2: 3};"#).parse_program()
-    );
-    println!(
-        "{:#?}",
-        Parser::from(r#"{{{{{{{{{};};};};};};};};};"#).parse_program()
+        Parser::from(
+            r#"
+type Ret<T, U> = fn(T, U) -> object T: U;
+let x: fn<T, U>(T, U) -> Ret<T, U> = fn<T, U>(x: T, y: U) -> fn() -> object T: U[] {
+    return fn() -> object T: U[] {
+        return object { "foo": x * y };
+    };
+};
+let y: object number: number[] = x(10, 20)();
+        "#
+        )
+        .parse_program()
     );
 }
