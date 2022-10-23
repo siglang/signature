f2b592bcea570d9f5decc0fc9261f916844075c0
tsukiroku <tsukiroku@yahoo.com>
2022-10-23 11:40:34 +0900

    Update main.rs

--- sntkc/src/main.rs
+++ sntkc/src/main.rs
@@ -53,33 +53,80 @@ fn main() {
 
     // a = 5 + 2; // a = 7
     // print(a); // prints 7
-    // b = a * 10; // b = 70
-    // print(b + a); // prints 77
+    // foo(a, 10, 20, "hello" + "world");
+    // a = (a + 1) * 2; // a = 16
+    // print(a); // prints 16
+    // Interpreter::new(
+    //     vec![
+    //         /* Line 1 */ Instruction::LoadConst(0), // 5
+    //         /* Line 1 */ Instruction::LoadConst(1), // 2
+    //         /* Line 1 */ Instruction::BinaryOp(BinaryOp::Add), // 5 + 2
+    //         /* Line 1 */ Instruction::StoreName(0), // a = 7
+    //         /* Line 2 */ Instruction::LoadName(0), // a, 7
+    //         /* Line 2 */ Instruction::LoadGlobal(1), // print
+    //         /* Line 2 */ Instruction::CallFunction(1), // print(7)
+    //         /* Line 3 */ Instruction::LoadName(0), // a, 7
+    //         /* Line 3 */ Instruction::LoadConst(2), // 10
+    //         /* Line 3 */ Instruction::LoadConst(3), // 20
+    //         /* Line 3 */ Instruction::LoadConst(4), // "hello"
+    //         /* Line 3 */ Instruction::LoadConst(5), // "world"
+    //         /* Line 3 */ Instruction::BinaryOp(BinaryOp::Add), // "hello" + "world"
+    //         /* Line 3 */ Instruction::LoadGlobal(2), // foo
+    //         /* Line 3 */ Instruction::CallFunction(4), // foo(7, 10, 20, "helloworld")
+    //         /* Line 4 */ Instruction::LoadName(0), // a, 7
+    //         /* Line 4 */ Instruction::LoadConst(1), // 2
+    //         /* Line 4 */ Instruction::BinaryOp(BinaryOp::Add), // 7 + 2
+    //         /* Line 4 */ Instruction::LoadConst(6), // 2
+    //         /* Line 4 */ Instruction::BinaryOp(BinaryOp::Mul), // (7 + 2) * 2
+    //         /* Line 4 */ Instruction::StoreName(0), // a = 16
+    //         /* Line 5 */ Instruction::LoadName(0), // a, 16
+    //         /* Line 5 */ Instruction::LoadGlobal(1), // print
+    //         /* Line 5 */ Instruction::CallFunction(1), // print(16)
+    //     ],
+    //     vec![
+    //         Value::LiteralValue(LiteralValue::Number(5.0)),
+    //         Value::LiteralValue(LiteralValue::Number(2.0)),
+    //         Value::LiteralValue(LiteralValue::Number(10.0)),
+    //         Value::LiteralValue(LiteralValue::Number(20.0)),
+    //         Value::LiteralValue(LiteralValue::String("hello".to_string())),
+    //         Value::LiteralValue(LiteralValue::String("world".to_string())),
+    //         Value::LiteralValue(LiteralValue::Number(2.0)),
+    //     ],
+    //     vec![
+    //         "a".to_string(),
+    //         "print".to_string(),
+    //         "foo".to_string(),
+    //     ],
+    // ).run();
+
+    // if (true) {
+    //   print("Hello, World!");
+    // } else {
+    //   print("Goodbye, World!");
+    // }
+    // print("Foo");
     Interpreter::new(
         vec![
-            /* Line 1 */ Instruction::LoadConst(0), // 5
-            /* Line 1 */ Instruction::LoadConst(1), // 2
-            /* Line 1 */ Instruction::BinaryOp(BinaryOp::Add), // 5 + 2
-            /* Line 1 */ Instruction::StoreName(0), // a = 7
-            /* Line 2 */ Instruction::LoadName(0), // a, 7
-            /* Line 2 */ Instruction::LoadGlobal(0), // print
-            /* Line 2 */ Instruction::CallFunction(1), // print(7)
-            /* Line 3 */ Instruction::LoadName(0), // a, 7
-            /* Line 3 */ Instruction::LoadConst(2), // 10
-            /* Line 3 */ Instruction::BinaryOp(BinaryOp::Mul), // 7 * 10
-            /* Line 3 */ Instruction::StoreName(1), // b = 70
-            /* Line 4 */ Instruction::LoadName(1), // b, 70
-            /* Line 4 */ Instruction::LoadName(0), // a, 7
-            /* Line 4 */ Instruction::BinaryOp(BinaryOp::Add), // 70 + 7
-            /* Line 4 */ Instruction::LoadGlobal(0), // print
-            /* Line 4 */ Instruction::CallFunction(1), // print(77)
+            /* 0  */ /* Line 1 */ Instruction::LoadConst(0), // true
+            /* 1  */ /* Line 1 */ Instruction::JumpIfFalse(5), // if (true) {
+            /* 2  */ /* Line 2 */ Instruction::LoadConst(1), // "Hello, World!"
+            /* 3  */ /* Line 2 */ Instruction::LoadGlobal(0), // print
+            /* 4  */ /* Line 2 */ Instruction::CallFunction(1), // print("Hello, World!")
+            /* 5  */ /* Line 3 */ Instruction::Jump(8), // } else {
+            /* 6  */ /* Line 4 */ Instruction::LoadConst(2), // "Goodbye, World!"
+            /* 7  */ /* Line 4 */ Instruction::LoadGlobal(0), // print
+            /* 8  */ /* Line 4 */ Instruction::CallFunction(1), // print("Goodbye, World!")
+            /* 9  */ /* Line 6 */ Instruction::LoadConst(3), // "Foo"
+            /* 10 */ /* Line 6 */ Instruction::LoadGlobal(0), // print
+            /* 11 */ /* Line 6 */ Instruction::CallFunction(1), // print("Foo")
         ],
         vec![
-            Value::LiteralValue(LiteralValue::Number(5.0)),
-            Value::LiteralValue(LiteralValue::Number(2.0)),
-            Value::LiteralValue(LiteralValue::Number(10.0)),
+            Value::LiteralValue(LiteralValue::Boolean(true)),
+            Value::LiteralValue(LiteralValue::String("Hello, World!".to_string())),
+            Value::LiteralValue(LiteralValue::String("Goodbye, World!".to_string())),
+            Value::LiteralValue(LiteralValue::String("Foo".to_string())),
         ],
-        vec!["print".to_string(), "a".to_string(), "b".to_string()],
+        vec!["print".to_string()],
     )
     .run();
 }
