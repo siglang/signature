833b576460a5d7b8f5d56fad89cfa699ffe9e759
tsukiroku <tsukiroku@yahoo.com>
2022-10-23 11:56:24 +0900

    Update code.rs

--- sntk_bytecode/src/code.rs
+++ sntk_bytecode/src/code.rs
@@ -5,6 +5,8 @@ macro_rules! instruction {
         pub enum Instruction {
             $( $op(usize) ),*,
             BinaryOp(BinaryOp),
+            BinaryOpEq(BinaryOpEq),
+            UnaryOp(UnaryOp),
         }
     };
 }
@@ -19,5 +21,27 @@ macro_rules! binary_op {
     }
 }
 
+macro_rules! binary_op_eq {
+    ($( $op:ident )*) => {
+        /// **Binary operations**
+        #[derive(Debug, PartialEq, Clone)]
+        pub enum BinaryOpEq {
+            $( $op ),*
+        }
+    }
+}
+
+macro_rules! unary_op {
+    ($( $op:ident )*) => {
+        /// **Unary operations**
+        #[derive(Debug, PartialEq, Clone)]
+        pub enum UnaryOp {
+            $( $op ),*
+        }
+    }
+}
+
 instruction! { LoadGlobal LoadConst CallFunction StoreName LoadName DeleteName JumpIfTrue JumpIfFalse Jump }
 binary_op! { Add Sub Mul Div Mod }
+binary_op_eq! { Eq Neq Lt Gt Lte Gte }
+unary_op! { Not Minus }
