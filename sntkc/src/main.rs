8934ca0cd9355d9cb8d11237d691afa720ca17df
tsukiroku <tsukiroku@yahoo.com>
2022-10-25 22:04:01 +0900

    Update main.rs

--- sntkc/src/main.rs
+++ sntkc/src/main.rs
@@ -9,31 +9,42 @@ use sntk_compiler::compiler::{Compiler, CompilerTrait};
 use sntk_core::parser::parser::{Parser, ParserTrait};
 
 fn main() {
-    // let mut compiler = Compiler::new(Parser::from("let a: number = 5; print(a);").parse_program());
-    // let compiled = compiler.compile_program();
+    let mut compiler = Compiler::new(
+        Parser::from(
+            r#"
+let x: number = 10;
+let y: number = 20;
+let z: number = x + y;
+print(x + y + z, x, y, z);
+    "#,
+        )
+        .parse_program(),
+    );
+    let mut compiled = compiler.compile_program().unwrap();
     // println!("{:?}", compiled);
-    // compiled.unwrap().run();
+    compiled.run();
+    // println!("{:?}", compiled.environment);
 
-    let mut interpreter = Interpreter::new(vec![
-        Instruction::LoadConst(Value::LiteralValue(LiteralValue::Number(5.0))),
-        Instruction::LoadConst(Value::LiteralValue(LiteralValue::Number(10.0))),
-        Instruction::BinaryOp(BinaryOp::Add),
-        Instruction::StoreName("a".to_string()),
-        Instruction::LoadName("a".to_string()),
-        Instruction::LoadGlobal("print".to_string()),
-        Instruction::CallFunction(1),
-        Instruction::LoadName("a".to_string()),
-        Instruction::StoreName("b".to_string()),
-        Instruction::LoadName("a".to_string()),
-        Instruction::LoadConst(Value::LiteralValue(LiteralValue::String("*".to_string()))),
-        Instruction::LoadName("b".to_string()),
-        Instruction::LoadConst(Value::LiteralValue(LiteralValue::String("is".to_string()))),
-        Instruction::LoadName("a".to_string()),
-        Instruction::LoadName("b".to_string()),
-        Instruction::BinaryOp(BinaryOp::Mul),
-        Instruction::LoadGlobal("print".to_string()),
-        Instruction::CallFunction(5),
-    ]);
+    // let mut interpreter = Interpreter::new(vec![
+    //     Instruction::LoadConst(Value::LiteralValue(LiteralValue::Number(5.0))),
+    //     Instruction::LoadConst(Value::LiteralValue(LiteralValue::Number(10.0))),
+    //     Instruction::BinaryOp(BinaryOp::Add),
+    //     Instruction::StoreName("a".to_string()),
+    //     Instruction::LoadName("a".to_string()),
+    //     Instruction::LoadGlobal("print".to_string()),
+    //     Instruction::CallFunction(1),
+    //     Instruction::LoadName("a".to_string()),
+    //     Instruction::StoreName("b".to_string()),
+    //     Instruction::LoadName("a".to_string()),
+    //     Instruction::LoadConst(Value::LiteralValue(LiteralValue::String("*".to_string()))),
+    //     Instruction::LoadName("b".to_string()),
+    //     Instruction::LoadConst(Value::LiteralValue(LiteralValue::String("is".to_string()))),
+    //     Instruction::LoadName("a".to_string()),
+    //     Instruction::LoadName("b".to_string()),
+    //     Instruction::BinaryOp(BinaryOp::Mul),
+    //     Instruction::LoadGlobal("print".to_string()),
+    //     Instruction::CallFunction(5),
+    // ]);
 
-    interpreter.run();
+    // interpreter.run();
 }
