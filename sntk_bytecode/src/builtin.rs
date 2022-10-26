fe07646de28fed81291db9966d825bc357975b1e
tsukiroku <tsukiroku@yahoo.com>
2022-10-26 21:30:30 +0900

    Update builtin.rs

--- sntk_bytecode/src/builtin.rs
+++ sntk_bytecode/src/builtin.rs
@@ -1,24 +1,22 @@
 use crate::stack::*;
 
-pub fn get_builtin(name: String) -> Option<impl FnOnce(Option<Vec<Value>>) -> Value> {
+pub fn get_builtin(name: String) -> Option<impl FnOnce(Vec<Value>) -> Value> {
     match name.clone().as_str() {
         "print" => Some(Print::call),
         _ => None,
     }
 }
 
-/// Built in function based trait.
-pub trait BuiltInTrait {
-    fn call(arguments: Option<Vec<Value>>) -> Value;
+pub trait BuiltIn {
+    fn call(arguments: Vec<Value>) -> Value;
 }
 
 pub struct Print;
 
-impl BuiltInTrait for Print {
-    fn call(arguments: Option<Vec<Value>>) -> Value {
-        if let Some(arguments) = arguments {
-            println!("{:?}", arguments);
-        }
+impl BuiltIn for Print {
+    fn call(arguments: Vec<Value>) -> Value {
+        println!("{}", arguments.iter().map(|x| format!("{} ", x)).collect::<String>());
+
         Value::None
     }
 }
