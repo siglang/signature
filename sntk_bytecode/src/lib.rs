f598f3b49f36f6533af72ea1bab3e86cf5c8e01c
tsukiroku <tsukiroku@yahoo.com>
2022-10-27 20:41:48 +0900

    Update lib.rs

--- sntk_bytecode/src/lib.rs
+++ sntk_bytecode/src/lib.rs
@@ -2,11 +2,9 @@
 pub mod builtin;
 /// **Instruction commands, binary operations**
 pub mod code;
-/// The error module contains the error handling for the bytecode runtime.
+/// **The error module contains the error handling for the bytecode runtime.**
 pub mod error;
 /// **Virtual machine interpreter.** it works on a stack basis.
 pub mod interpreter;
-/// **Stack implementation.**
-///
-/// The stack on which the interpreter is based.
+/// **The stack on which the interpreter is based.**
 pub mod stack;
