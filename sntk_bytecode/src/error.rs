9a93c3ead1458f78c3d80c29c22491ccd21d8e6b
tsukiroku <tsukiroku@yahoo.com>
2022-10-27 20:41:44 +0900

    Update error.rs

--- sntk_bytecode/src/error.rs
+++ sntk_bytecode/src/error.rs
@@ -3,11 +3,11 @@ use std::*;
 #[derive(Debug, PartialEq, Default, Clone)]
 pub struct ByteCodeRuntime {
     pub message: String,
-    pub pointer: usize,
+    pub pointer: Option<usize>,
 }
 
 impl ByteCodeRuntime {
-    pub fn new(message: &str, args: Vec<String>, pointer: usize) -> Self {
+    pub fn new(message: &str, args: Vec<String>, pointer: Option<usize>) -> Self {
         let mut message = message.to_string();
 
         args.iter().enumerate().for_each(|(i, arg)| {
@@ -19,16 +19,18 @@ impl ByteCodeRuntime {
 }
 
 impl fmt::Display for ByteCodeRuntime {
+    #[rustfmt::skip]
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(f, "{} (on {})", self.message, self.pointer)
+        write!(f, "{} {}", self.message, match self.pointer {
+            Some(p) => format!("(at {})", p),
+            None => "".to_string()
+        })
     }
 }
 
 macro_rules! messages {
     ($( $name:ident => $message:expr );*;) => {
-        $(
-            pub const $name: &str = $message;
-        )*
+        $( pub const $name: &str = $message; )*
     };
 }
 
@@ -36,13 +38,18 @@ messages! {
     NOT_DEFINED => "Name '{0}' is not defined";
     NOT_A_FUNCTION => "Not a function: {0}";
     NOT_A_BOOLEAN => "Not a boolean: {0}";
+    NOT_A_LITERAL_VALUE => "Not a literal value: {0}";
     INVALID_OPERAND => "Invalid operand: {0}";
     UNKNOWN_FUNCTION => "Unknown function: {0}";
+    POP_EMPTY_STACK => "Pop empty stack";
 }
 
 #[macro_export]
 macro_rules! runtime_error {
     ($self:ident; $msg:ident; $( $r:expr ),*) => {
-        panic!("{}", ByteCodeRuntime::new($msg, vec![$( $r.to_string() ),*], $self.instruction_pointer))
+        panic!("{}", ByteCodeRuntime::new($msg, vec![$( $r.to_string() ),*], Some($self.instruction_pointer)))
+    };
+    (@stack $self:ident; $msg:ident; $( $r:expr ),*) => {
+        panic!("{}", ByteCodeRuntime::new($msg, vec![$( $r.to_string() ),*], None))
     };
 }
