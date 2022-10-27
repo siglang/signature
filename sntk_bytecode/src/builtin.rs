57de5d91bc1f4b2633fc4ccdeae1b2e625c598ee
tsukiroku <tsukiroku@yahoo.com>
2022-10-27 19:27:41 +0900

    Update builtin.rs

--- sntk_bytecode/src/builtin.rs
+++ sntk_bytecode/src/builtin.rs
@@ -3,7 +3,6 @@ use crate::stack::*;
 pub fn get_builtin(name: String) -> Option<impl FnOnce(Vec<Value>) -> Value> {
     match name.clone().as_str() {
         "print" => Some(Print::call_return()),
-        "foo" => Some(Foo::call_return()),
         _ => None,
     }
 }
@@ -20,16 +19,6 @@ pub struct Print;
 impl BuiltIn for Print {
     fn call(arguments: Vec<Value>) -> Value {
         println!("{}", arguments.iter().map(|x| format!("{} ", x)).collect::<String>());
-
-        Value::None
-    }
-}
-
-pub struct Foo;
-
-impl BuiltIn for Foo {
-    fn call(arguments: Vec<Value>) -> Value {
-        println!("Foo called with {} arguments", arguments.len());
 
         Value::LiteralValue(LiteralValue::Boolean(true))
     }
