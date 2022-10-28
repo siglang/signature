fb20b0d0156e02d7718d9a07f17ad13adea518e4
tsukiroku <tsukiroku@yahoo.com>
2022-10-28 19:45:03 +0900

    Update main.rs

--- sntkc/src/main.rs
+++ sntkc/src/main.rs
@@ -1,40 +1,55 @@
 #![allow(unused_imports)]
 
+use sntk_compiler::compiler::{Compiler, CompilerTrait};
+use sntk_core::parser::parser::{Parser, ParserTrait};
 use sntk_ir::{
-    code::{BinaryOp, Instruction, BinaryOpEq},
+    code::{BinaryOp, BinaryOpEq, Block, Instruction},
     interpreter::{Interpreter, InterpreterBase},
     stack::{LiteralValue, Value},
 };
-use sntk_compiler::compiler::{Compiler, CompilerTrait};
-use sntk_core::parser::parser::{Parser, ParserTrait};
 
 fn main() {
-    //     let mut compiler = /*Compiler::new(*/
-    //         Parser::from(
-    //             r#"
-    // if 3 > 2 {
-    //     print("hello world");
-    //     return true;
-    // };
-    //     "#.to_string(),
-    //         )
-    //         .parse_program();
-    //     // );
-    //     // let mut compiled = compiler.compile_program().unwrap();
-    //     // compiled.run();
-    //     println!("{:#?}", compiler);
+    let mut compiler = Compiler::new(
+        Parser::from(
+            r#"
+let a: number = 3;
+let b: number = 2;
 
-    let mut interpreter = Interpreter::new(vec![
-        Instruction::LoadConst(Value::LiteralValue(LiteralValue::Boolean(true))),
-        Instruction::JumpIfFalse(6),
-        Instruction::LoadConst(Value::LiteralValue(LiteralValue::String("hello world".to_string()))),
-        Instruction::LoadGlobal("print".to_string()),
-        Instruction::CallFunction(1),
-        Instruction::Jump(7),
-        Instruction::LoadConst(Value::LiteralValue(LiteralValue::String("hello world x".to_string()))),
-        Instruction::LoadGlobal("print".to_string()),
-        Instruction::CallFunction(1),
-    ]);
+let x: number = if a < b {
+    print("a < b");
 
-    interpreter.run();
+    return 1;
+} else if a > b {
+    print("a > b");
+
+    return 2;
+    print("This will not be printed");
+} else {
+    print("a == b");
+
+    return 3;
+};
+
+print(x);
+            "#
+            .to_string(),
+        )
+        .parse_program(),
+    );
+    let mut compiled = compiler.compile_program().unwrap();
+    compiled.run();
+
+    // let mut interpreter = Interpreter::new(vec![
+    //     Instruction::LoadConst(Value::LiteralValue(LiteralValue::Boolean(false))),
+    //     Instruction::If(
+    //         Block(vec![
+    //             Instruction::LoadConst(Value::LiteralValue(LiteralValue::String("hello world".to_string()))),
+    //             Instruction::LoadGlobal("print".to_string()),
+    //             Instruction::CallFunction(1),
+    //         ]),
+    //         None,
+    //     ),
+    // ]);
+
+    // interpreter.run();
 }
