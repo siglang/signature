35c6a5ac717d8f5069ff82dfad6c31080b379879
tsukiroku <tsukiroku@yahoo.com>
2022-10-28 18:07:44 +0900

    Create lib.rs

---
+++ sntk_ir/src/lib.rs
@@ -0,0 +1,10 @@
+/// **Built-in functions.**
+pub mod builtin;
+/// **Instruction commands, binary operations**
+pub mod code;
+/// **The error module contains the error handling for the Sanetaka IR runtime.**
+pub mod error;
+/// **Virtual machine interpreter.** it works on a stack basis.
+pub mod interpreter;
+/// **The stack on which the interpreter is based.**
+pub mod stack;
