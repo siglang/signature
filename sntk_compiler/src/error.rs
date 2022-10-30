a659c4446d1feb084bc1b4783593c6715c26aae7
tsukiroku <tsukiroku@yahoo.com>
2022-10-30 10:49:07 +0900

    Update error.rs

--- sntk_compiler/src/error.rs
+++ sntk_compiler/src/error.rs
@@ -1,9 +1,16 @@
 use sntk_core::parser::error::ParsingError;
 
 #[derive(Debug, Clone)]
-pub enum CompileError {
-    /// The error type for the compiler.
-    CompileError(String),
-    /// The error type for the parser.
-    ParseError(Vec<ParsingError>),
+pub struct CompileError(pub Vec<ParsingError>);
+
+impl std::fmt::Display for CompileError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut messages = String::new();
+
+        for ParsingError { message, position } in &self.0 {
+            messages.push_str(&format!("Parsing error: {} at line {}, column {}\n", message, position.0, position.1));
+        }
+
+        write!(f, "{}", messages.trim_end())
+    }
 }
