8bf48f0ad6605bc653f7814316a00f96cec0504a
tsukiroku <tsukiroku@yahoo.com>
2022-11-05 15:34:32 +0900

    `remove`: removed IR

--- sntk_ir/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod builtin;
-pub mod code;
-pub mod error;
-pub mod interpreter;
-pub mod stack;
-pub mod value;
--- sntkc/src/main.rs
+++ sntkc/src/main.rs
@@ -1,28 +1,27 @@
-use std::time::Instant;
-
-use sntk_compiler::compiler::{Compiler, CompilerTrait};
-use sntk_core::parser::parser::{Parser, ParserTrait};
-use sntk_ir::interpreter::InterpreterBase;
+// use std::time::Instant;
+// use sntk_compiler::compiler::{Compiler, CompilerTrait};
+// use sntk_core::parser::parser::{Parser, ParserTrait};
+// use sntk_ir::interpreter::InterpreterBase;
 
 fn main() {
-    let parsed = Parser::from(
-        r#"
-auto x = fn(a: number, b: string) -> 
-    fn() -> string ->
-        fn() -> string[] -> [typeof a, typeof b];
+    //     let parsed = Parser::from(
+    //         r#"
+    // auto x = fn(a: number, b: string) ->
+    //     fn() -> string ->
+    //         fn() -> string[] -> [typeof a, typeof b];
 
-print(x(1, "hello")(), "typeof:", typeof x);
-"#
-        .to_string(),
-    )
-    .parse_program();
+    // print(x(1, "hello")(), "typeof:", typeof x);
+    // "#
+    //         .to_string(),
+    //     )
+    //     .parse_program();
 
-    let start_time = Instant::now();
+    //     let start_time = Instant::now();
 
-    match &mut Compiler::new(parsed).compile_program() {
-        Ok(interpreter) => interpreter.run(),
-        Err(error) => println!("{error}"),
-    }
+    //     match &mut Compiler::new(parsed).compile_program() {
+    //         Ok(interpreter) => interpreter.run(),
+    //         Err(error) => println!("{error}"),
+    //     }
 
-    println!("Time: {:?}s", start_time.elapsed().as_secs_f64());
+    //     println!("Time: {:?}s", start_time.elapsed().as_secs_f64());
 }
