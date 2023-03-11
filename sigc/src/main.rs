19547ade29efcb8f433c159b21622caf8e748a15
ky0422 <ky0422a@gmail.com>
2023-03-11 19:22:51 +0900

    `update`: updated Signature

--- sigc/src/main.rs
+++ sigc/src/main.rs
@@ -1,51 +1,15 @@
-use clap::Parser as _;
-pub mod arguments;
+mod arguments;
+use std::fs;
 
-use crate::arguments::Cli;
-use compiler::compiler::Compiler;
-use ir_interpreter::interpreter::IrInterpreter;
-use parser::{parser::parser::Parser, tokenizer::lexer::Lexer};
-use std::{fs::read_to_string, time::Instant};
+use clap::Parser as _;
+use parser::{parser::Parser, tokenizer::Lexer};
 
 fn main() {
-    let Cli {
-        source,
-        debug,
-        .. // todo
-    } = Cli::parse();
+    let args = arguments::Cli::parse();
+    let content = fs::read_to_string(args.source).unwrap();
 
-    let source_code = match read_to_string(source) {
-        Ok(source) => source,
-        Err(e) => panic!("Error reading source file: {}", e),
-    };
+    let lexer = Lexer::new(content);
+    let mut parser = Parser::new(lexer);
 
-    let mut start = Instant::now();
-
-    let lexer = Lexer::new(source_code);
-    let parsed = Parser::new(lexer).parse_program();
-
-    if debug {
-        println!("Parsing Elapsed: {}s", start.elapsed().as_secs_f64())
-    }
-
-    match Compiler::new(parsed).compile_program() {
-        Ok(instructions) => {
-            if debug {
-                println!("Compiling Elapsed: {}s", start.elapsed().as_secs_f64());
-            }
-            start = Instant::now();
-
-            let mut ir_interpreter = IrInterpreter::new(instructions);
-
-            match ir_interpreter.eval() {
-                Ok(_) => {
-                    if debug {
-                        println!("Interpreting Elapsed: {}s", start.elapsed().as_secs_f64());
-                    }
-                }
-                Err(error) => println!("{:?}", error),
-            }
-        }
-        Err(e) => println!("{e}"),
-    }
+    println!("{:#?}", parser.parse_program());
 }
